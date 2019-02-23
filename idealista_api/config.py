741734bb6fa2ec308a3f81f7569bac4feec8b4e3
miguel.torres.porta <miguel.torres.porta@bbva.com>
2019-02-23 19:55:30 +0200

    remove ids

--- idealista_api/config.py
+++ idealista_api/config.py
@@ -1,7 +1,7 @@
 # API Credentials provided by Idealista
 API_CREDENTIALS = {
-    "api_key": "soaek9ob0jv9984d58qqanmjmj91gaws",
-    "secret": "TziImiLTvEsA"
+    "api_key": "key",
+    "secret": "secret"
 }
 
 USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0"
