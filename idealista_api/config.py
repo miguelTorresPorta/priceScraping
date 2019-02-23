a6c515d4fb7223ffd5448838fea09e94e3e1f6c5
j4elguero <j4elguero@gmail.com>
2019-02-23 16:45:06 +0200

    Remove personal keys.. oops

--- idealista_api/config.py
+++ idealista_api/config.py
@@ -1,7 +1,7 @@
 # API Credentials provided by Idealista
 API_CREDENTIALS = {
-    "api_key": "wronsda00j8iwa8zyokfuu8yunvhd5ud",
-    "secret": "AXhFIfry2MSW"
+    "api_key": "your_api_key",
+    "secret": "your_secret"
 }
 
 USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0"
